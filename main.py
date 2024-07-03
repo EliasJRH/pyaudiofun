841199902c2693ec5cc6a35b66052457dc6e838c
Elias Hawa <elias.hawa2468@gmail.com>
2024-07-02 22:43:32 -0400

    Add algorithm for determining note delays

--- main.py
+++ main.py
@@ -1,23 +1,61 @@
 # import numpy as np
 # from scipy import signal    
+from functools import cache
 import pygame
 import math
 import threading
 import sys
 import xml.etree.ElementTree as ET
+import time
+
+notes_to_beat = {
+    "whole": 4,
+    "half": 2,
+    "quarter": 1,
+    "eighth": 0.5,
+    "16th": 0.25,
+}
+
+@cache
+def find_tempo(element):
+    try:
+        return element.find("direction").find("direction-type").find("metronome").find("per-minute").text
+    except AttributeError:
+        return None
 
 def parse_musicxml(file):
     tree = ET.parse(file)
-    print(tree)
     measures = tree.find("part").findall("measure")
-    for m in measures:
-        last_x = 0
-        print(m)
-        items = m.iterfind("*")
+    note_delays_seconds = []
+    last_second = 0
+    for n, m in enumerate(measures):
+        last_x = 0 # Used to distinguish notes from each other
+        items = m.iterfind("*") # Used to find all notes
+        tempo = int(find_tempo(m)) if find_tempo(m) else (tempo if tempo else 0) # Used to determine length of each note
+        spb = 60 / tempo # How many seconds each beat should last
         for i in items:
+            # We only want to look at the primary notes, so we ignore the backup notes
             if i.tag == "note":
-                print(i.get("default-x"))
+                # TODO: Check if note is a rest or reg note
+                # TODO: Check if note is a swing note
+                x_pos = float(i.get("default-x"))
+                # Some notes are stacked, so we only look at it if it's positioned differently
+                if x_pos != last_x:
+                  type = i.find("type").text # Type of note (whole, half, etc)
+                  dot = True if i.find("dot") is not None else False # If the note is dotted, we'll need to add an additional half of its length
+                  last_x = x_pos
+                  note_delays_seconds.append((n + 1, last_second))
+                  last_second += (notes_to_beat[type] + (notes_to_beat[type] / 2 if dot else 0)) * spb # Update time delay
             elif i.tag == "backup": break
+    return note_delays_seconds
+
+def print_beat_changes(note_times):
+    last_second = 0
+    for i in note_times:
+        print(i - last_second)
+        last_second = i
+        time.sleep(i - last_second)
+        print(i)
 
 def run_game():
     pygame.init()
@@ -49,8 +87,9 @@ def run_game():
     run = True
     while run:
 
+        
         clock.tick(FPS)
-
+        pygame.time.get_ticks()
         #draw scrolling background
         for i in range(0, tiles):
             screen.blit(bg, (i * bg_width + scroll, 0))
@@ -92,12 +131,18 @@ def run_game():
     pygame.quit()
 
 def main():
-    parse_musicxml("Gravity_Falls_Opening_-_Intermediate_Piano_Solo.musicxml")
+    # note_times = parse_musicxml("Gravity_Falls_Opening_-_Intermediate_Piano_Solo.musicxml")
+    # print(note_times)
+    note_times = parse_musicxml("Phineas_and_ferb_theme_–_Bowling_for_Soup_Phineas_and_Ferb_-_Theme_song.musicxml")
+    print(note_times)
     # game_thread = threading.Thread(target=run_game)
+    # beat_thread = threading.Thread(target=print_beat_changes, args=(note_times,))
 
     # game_thread.start()
+    # beat_thread.start()
 
     # game_thread.join()
+    # beat_thread.join()
 
 if __name__ == "__main__":
     main()
